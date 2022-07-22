c9d0bc508b4b57d899a5f55f883b326378b38fc3
Devanand1794 <devanand1794@gmail.com>
2022-07-22 16:04:17 +0530

    megala code

--- src/org/test/testdeva.java
+++ src/org/test/testdeva.java
@@ -2,4 +2,29 @@ package org.test;
 
 public class testdeva {
 
+	
+	public void test1() {
+		System.out.println("test 1 is fully completed and satisfied");
+
+	}
+	public void test2() {
+		System.out.println("test 2 is fully completed and satisfied");
+		
+	}
+	private void test3() {
+		System.out.println("test 3 is fully completed and satisfied");
+		
+	}
+	public void test4() {
+		System.out.println("test 4 is not fully completed and satisfied");
+		
+	}
+	public void test10() {
+		System.out.println("test 5 is not fully completed and satisfied");
+		
+	}
+	public void test6() {
+		System.out.println("test 6 is fully completed and satisfied");
+		
+	}
 }
